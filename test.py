563e98f67e29d409596afce6dfbb261c2be6025c
arturbajan <artur.bajan@gmail.com>
2022-09-19 19:59:59 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
-
 import streamlit as st
-import pandas as pd
 
-st.header("Witaj")
-st.write('wczytaj')
+st.header("Witaj w aplikacji analitycznej")
+st.write("Możesz wybrać plik csv do analizy")
+
+plik = st.file_uploader('Wybierz CSV')
+
+try:
+	if st.button("Pokaż CSV" 😞
+		import pandas as pd
+		df = pd.read_csv(plik)
+
+		st.dataframe(df)
+
+except ValueError:
+
+	st.write("Nie wybrałeś pliku")
