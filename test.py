54768342f15db4498cb34fd8e20d7629eb5decf2
arturbajan <artur.bajan@gmail.com>
2022-09-19 19:42:41 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import streamlit as st
+'''import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -6,3 +6,14 @@ df=pd.read_csv("https://stooq.pl/q/d/l/?s=eurpln&i=d")
 df1=df[["Data","Otwarcie"]].set_index("Data").sort_index(ascending=False)
 #df1.plot()
 st.pyplot(df1)
+'''
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
