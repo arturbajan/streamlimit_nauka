f8307a51cea316e62c28c6c827fb7578016bb2ea
arturbajan <artur.bajan@gmail.com>
2022-09-19 19:25:14 +0200

    1
    1

---
+++ test.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+df=pd.read_csv("https://stooq.pl/q/d/l/?s=eurpln&i=d")
+df1=df[["Data","Otwarcie"]].set_index("Data").sort_index(ascending=False)
+#df1.plot()
+st.write(df1)
