d8c1d47f058069c70ba0db9abfc81fb0cbbfeef5
arturbajan <artur.bajan@gmail.com>
2022-09-19 19:34:51 +0200

    Update test.py
    3

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 df=pd.read_csv("https://stooq.pl/q/d/l/?s=eurpln&i=d")
 df1=df[["Data","Otwarcie"]].set_index("Data").sort_index(ascending=False)
-df1.plot()
-#st.write(df1)
+#df1.plot()
+st.write(df1)
