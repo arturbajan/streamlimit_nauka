8dbff437cad1dbf40412f1d29cd6b460b70b6e27
arturbajan <artur.bajan@gmail.com>
2022-09-19 20:29:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from tools  import *
+import pandas as pd
 
 st.header("Witaj w aplikacji analitycznej")
 st.write("Możesz wybrać plik csv do analizy")
@@ -8,11 +9,11 @@ plik = st.file_uploader('Wybierz CSV')
 
 try:
 	if st.button("Pokaż CSV"): 
-		import pandas as pd
+		
 		st.dataframe(tools.read_cvs(plik))
 		#df = pd.read_csv(plik)
 
-		st.dataframe(df)
+		#st.dataframe(df)
 
 except ValueError:
 
