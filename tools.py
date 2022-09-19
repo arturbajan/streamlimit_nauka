5d9c3277d7870dd3143acc482f91a631e899553a
arturbajan <artur.bajan@gmail.com>
2022-09-19 20:26:41 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -4,3 +4,4 @@ def read_csv(plik):
 
 	df = pd.read_csv(plik)
 	df = df[['work_year','experience_level','employment_type','job_title','salary_in_usd','employee_residence','remote_ratio','company_location','company_size' ]]
+	return df
